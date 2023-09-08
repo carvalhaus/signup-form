708d41c20c805825b9e9e88447a8f277f1bd52ff
carvalhaus <ooliveira.joaop@gmail.com>
2023-09-07 21:38:59 -0300

    feat: form style update

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -4,8 +4,27 @@ const Form = () => {
   return (
     <form>
       <label>
-        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="First Name" />
       </label>
+
+      <label>
+        <input type="text" placeholder="Last Name" />
+      </label>
+
+      <label>
+        <input type="email" placeholder="Email Address" />
+      </label>
+
+      <label>
+        <input type="password" placeholder="Password" />
+      </label>
+
+      <button type="submit">Claim your free trial</button>
+
+      <small>
+        By clicking the button you are agreeing to ours{" "}
+        <a href="#">Terms and Services</a>
+      </small>
     </form>
   );
 };
